da4a59de8efbedbca496e4f671e7f0c0568543e9
ZheFrench <jpvillemin@gmail.com>
2019-10-03 23:04:42 +0200

    Update installer.R

--- installer.R
+++ installer.R
@@ -2,4 +2,4 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     	install.packages("BiocManager")
 BiocManager::install()
 
-BiocManager::install(c("karyoploteR","rtracklayer"),ask = FALSE)
+BiocManager::install(c("karyoploteR","rtracklayer","singscore"),ask = FALSE)
