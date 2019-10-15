fcf31f35d5f834d27422f178afef9db354dca516
ZheFrench <jpvillemin@gmail.com>
2019-10-15 11:34:23 +0200

    add optparse

--- installer.R
+++ installer.R
@@ -2,4 +2,4 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     	install.packages("BiocManager")
 BiocManager::install(ask=FALSE)
 
-BiocManager::install(c("karyoploteR","rtracklayer","singscore"),update = TRUE,ask = FALSE)
+BiocManager::install(c("karyoploteR","rtracklayer","singscore","optparse"),update = TRUE,ask = FALSE)
