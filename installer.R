86c1a626aa09ae2777138e121a0501592398e6ff
ZheFrench <jpvillemin@gmail.com>
2019-10-23 12:55:42 +0200

    Update installer.R

--- installer.R
+++ installer.R
@@ -2,4 +2,5 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     	install.packages("BiocManager")
 BiocManager::install(ask=FALSE)
 
-BiocManager::install(c("karyoploteR","rtracklayer","singscore","optparse"),update = TRUE,ask = FALSE)
+BiocManager::install(c("karyoploteR","rtracklayer","singscore","optparse","survminer" \
+                       ,"survival","gridExtra","stringr","tidyr","plyr","reshape2","data.table"),update = TRUE,ask = FALSE)
